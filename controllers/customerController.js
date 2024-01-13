ec3aad15c788b0539ac82ba9293e5be2e325113d
lj140542 <jonathan.lefebvre76600@gmail.com>
2024-01-13 15:00:59 +0100

    Implements Customers CRUD functions

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,7 @@
 const Customer = require("../models/customer");
 const Mission = require('../models/mission');
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // displays all customers
 exports.customer_list = asyncHandler(async (req, res, next) => {
@@ -37,30 +38,130 @@ exports.customer_detail = asyncHandler(async (req, res, next) => {
 
 // display customer create form on GET
 exports.customer_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Customer create GET");
+  res.render('customer_form', { title: "Create customer" });
 });
 
 // handle customer create form on POST
-exports.customer_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Customer create POST");
-});
+exports.customer_create_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Name has non-alphanumeric characters.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const customer = new Customer({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      res.render('customer_form', {
+        title: "Create customer",
+        customer: customer,
+        errors: errors
+      });
+      return;
+    }
+    else {
+      await customer.save();
+      res.redirect(customer.url);
+    }
+  })
+];
 
 // display customer update form on GET 
 exports.customer_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Customer update GET");
+  const customer = await Customer.findById(req.params.id).exec();
+
+  if (customer == null) {
+    let err = new Error('Customer not found');
+    err.status = 404;
+    next(err);
+  }
+
+  res.render('customer_form', {
+    title: 'Update customer',
+    customer: customer
+  });
 });
 
 // handler customer update form on POST
-exports.customer_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Customer update POST");
-});
+exports.customer_update_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Name has non-alphanumeric characters.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const customer = new Customer({
+      name: req.body.name,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('customer_form', {
+        title: "Create customer",
+        customer: customer,
+        errors: errors
+      });
+      return;
+    }
+    else {
+      const updatedCustomer = await Customer.findByIdAndUpdate(req.params.id, customer);
+      res.redirect(updatedCustomer.url);
+    }
+  })
+];
 
 // display customer delete form on GET
 exports.customer_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Customer delete GET");
+  const [customer, customerMissions] = await Promise.all([
+    Customer.findById(req.params.id).exec(),
+    Mission.find({ customer: req.params.id }).exec()
+  ]);
+
+  if (customer === null) {
+    let err = new Error('Customer not found');
+    err.status = 404;
+    next(err);
+  }
+
+  res.render('customer_delete', {
+    title: 'Delete customer',
+    customer: customer,
+    mission_list: customerMissions
+  });
 });
 
 // handle customer delete form on POST
 exports.customer_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Customer delete POST");
+  const [customer, customerMissions] = await Promise.all([
+    Customer.findById(req.params.id).exec(),
+    Mission.find({ customer: req.params.id }).exec()
+  ]);
+
+  if (customer === null) {
+    let err = new Error('Customer not found');
+    err.status = 404;
+    next(err);
+  }
+
+  if (customerMissions.length > 0) {
+    res.render('customer_delete', {
+      title: 'Delete customer',
+      customer: customer,
+      mission_list: customerMissions
+    });
+    return;
+  }
+
+  await Customer.findByIdAndDelete(req.params.id);
+  res.redirect('/schedule/customers');
 });
--- populatedb.js
+++ populatedb.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 console.log(
-  'This script populates some test customers, drivers, vehicles and missions to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
+  'This script populates some test customers, drivers, vehicles and missions to your database. Specified database as argument'
 );
 
 // Get arguments passed on command line
